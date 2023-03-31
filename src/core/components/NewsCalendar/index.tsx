3dc044a4473ee3168e02fafeaa2901857bb23274
Md Soykot <md.soykot2910@gmail.com>
2023-03-31 11:19:05 +0600

    calandar page

---
+++ src/core/components/NewsCalendar/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NewCalendar = () => {
+  return <div>NewCalendar:fdfffff.FC</div>;
+};
+
+export default NewCalendar;
---
+++ src/pages/news-calendar.tsx
@@ -0,0 +1,12 @@
+import NewCalendar from "@/core/components/NewsCalendar";
+import React from "react";
+
+const Newscalendar = () => {
+  return (
+    <>
+      <NewCalendar />
+    </>
+  );
+};
+
+export default Newscalendar;
