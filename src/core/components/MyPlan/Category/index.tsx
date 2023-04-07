4bfb52beec546eb9635dbed02f1a5d3f50b1a2eb
kamrul-hasan01 <k17h02@gmail.com>
2023-04-07 10:42:10 +0600

    select category section develop

---
+++ src/core/components/MyPlan/Category/index.tsx
@@ -0,0 +1,36 @@
+import { Col, Row } from "antd";
+import { useState } from "react";
+import Styles from "./Category.module.scss";
+const Category = () => {
+  const [currentCategory, setCurrentCategory] = useState("");
+  return (
+    <Row>
+      <Col span={24}>
+        <p className={Styles?.title}>Select you category</p>
+        <p className={Styles?.info}>
+          we will show you appropriate plan based on selection
+        </p>
+      </Col>
+      <Col>
+        <button
+          className={`${Styles.btnStyle} ${
+            currentCategory === "consistency" && Styles.activeBtn
+          }`}
+          onClick={() => setCurrentCategory("consistency")}
+        >
+          Consistency
+        </button>
+        <button
+          className={`${Styles.btnStyle} ${
+            currentCategory === "non-consistency" && Styles.activeBtn
+          }`}
+          onClick={() => setCurrentCategory("non-consistency")}
+        >
+          Non Consistency
+        </button>
+      </Col>
+    </Row>
+  );
+};
+
+export default Category;
--- src/core/components/MyPlan/MyPlanWrapper/index.tsx
+++ src/core/components/MyPlan/MyPlanWrapper/index.tsx
@@ -1,6 +1,9 @@
 import { Col, Row } from "antd";
+import Category from "../Category";
 import { MyPlanHero } from "../MyPlanHero";
-import { PricingContainer } from "../PricingContainer";
+import PlanInformation from "../PlanInformation";
+import Platform from "../PlatForm";
+import { PricingCard } from "../PricingCard";
 import Styles from "./MyPlanWrapper.module.scss";
 const MyPlanWrapper = () => {
   return (
@@ -12,9 +15,22 @@ const MyPlanWrapper = () => {
         <Col span={24}>
           <MyPlanHero />
         </Col>
-        <Col span={24}>
-          <PricingContainer />
-        </Col>
+
+        <Row gutter={100} align={"middle"} className={Styles?.pricingContainer}>
+          <Col span={12}>
+            {" "}
+            <Category />
+          </Col>
+          <Col span={12}>
+            <Platform />
+          </Col>
+          <Col span={11}>
+            <PricingCard />
+          </Col>
+          <Col span={13}>
+            <PlanInformation />
+          </Col>
+        </Row>
       </Row>
     </>
   );
