da40f8bc49b64b5dd7592bc7ec62a24796de525f
Md Soykot <md.soykot2910@gmail.com>
2023-05-11 15:57:31 +0600

    forget & reset password responsive done

--- src/core/components/ForgetPassword/Header/index.tsx
+++ src/core/components/ForgetPassword/Header/index.tsx
@@ -2,21 +2,22 @@ import React from "react";
 import Style from "../Header/Header.module.scss";
 import { Button } from "antd";
 import Link from "next/link";
+import { useMediaQuery } from "@/hooks/responsive.hooks";
 
 export const Header: React.FC = () => {
+  let isTab = useMediaQuery("(max-width: 1080px)");
   return (
     <div className={Style.wrapper}>
-      <img src="https://res.cloudinary.com/dvzadhnmh/image/upload/v1680061656/logo-login.svg" />
-      <div>
-        <Button
-          type="primary"
-          size="middle"
-          style={{ width: "160px", height: "40px" }}
-          className=""
-        >
+      {isTab ? (
+        <img src="https://res.cloudinary.com/dvzadhnmh/image/upload/v1683788663/fundednext-dashboard-v2/logo-icon-black.svg" />
+      ) : (
+        <img src="https://res.cloudinary.com/dvzadhnmh/image/upload/v1680061656/logo-login.svg" />
+      )}
+      <div className={Style.btnWrapper}>
+        <Button type="primary" size="middle" className={Style.btn}>
           <Link href="/register">Register</Link>
         </Button>
-        <Button style={{ width: "160px", height: "40px" }} className="ml-4">
+        <Button className={`ml-2 ${Style.btn}`}>
           <Link href="/login"> Log In</Link>
         </Button>
       </div>
